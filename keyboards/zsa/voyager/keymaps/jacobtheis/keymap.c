9a2d520c1b6fdb7c811f0a560823bda1cf71942e
jacobtheis <jtheis@nationalhomerentals.com>
2025-01-04 00:30:15 -0600

    Swapped the layer keys for speed
    Achordion seems to put a lot of preference on cross hand actions for
    speed. Updating the layers so that they make use of this.

--- keyboards/zsa/voyager/keymaps/jacobtheis/keymap.c
+++ keyboards/zsa/voyager/keymaps/jacobtheis/keymap.c
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESCAPE,      KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                                           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLS,
     KC_HYPR,        MT(MOD_LSFT, KC_A),MT(MOD_LCTL, KC_S),MT(MOD_RALT, KC_D),MT(MOD_LGUI, KC_F),KC_G,                                           KC_H,           MT(MOD_RGUI, KC_J),MT(MOD_RALT, KC_K),MT(MOD_RCTL, KC_L),MT(MOD_RSFT, KC_SCLN),KC_QUOTE,
     CW_TOGG,        KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_RIGHT_CTRL,
-                                                    LT(1,KC_ENTER), KC_TAB,                                         KC_BSPC,        LT(2,KC_SPACE)
+                                                    LT(2,KC_ENTER), KC_TAB,                                         KC_BSPC,        LT(1,KC_SPACE)
   ),
   [1] = LAYOUT_voyager(
     KC_ESCAPE,      KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,                                          KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,
